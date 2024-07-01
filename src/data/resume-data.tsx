f21a6b89036ca5c341e4d58ff766a5c0d0e9ce36
ebrardev <16155052@mersin.edu.tr>
2024-07-01 20:12:00 +0300

    data changes

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,113 +20,105 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Ebrar Altunkaynak",
+  initials: "",
+  location: "Ankara, Turkey",
+  locationLink: "",
   about:
-    "Detail-oriented Full Stack Engineer dedicated to building high-quality products.",
+    "Software engineer talk about web and mobile projects.",
   summary:
-    "As a Full Stack Engineer, I specialize in taking products from concept to launch. I excel in leading teams and creating environments where individuals perform at their best. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience working remotely with companies worldwide.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "A golang lover , I like coding with javascript libraries",
+  avatarUrl: "https://avatars.githubusercontent.com/u/73947103?v=4",
+  personalWebsiteUrl: "https://ebrarcode.dev",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "altunkaynakebrar@gmail.com",
+    tel: "+90 553 589 59 90",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/ebrardev",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/meadev/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/ebrardev",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Mersin university",
+      degree: "Bachelor's Degree in  Computer Engineering",
+      start: "2017",
+      end: "2023",
     },
   ],
   work: [
     {
-      company: "Film.io",
-      link: "https://film.io",
+      company: "Futurity Studio",
+      link: "hhttps://futuritystudio.com",
       badges: ["Remote"],
-      title: "Software Architect",
-      logo: ConsultlyLogo,
-      start: "2024",
-      end: null,
-      description:
-        "Leading the development of the Film.io platform. Technologies: React, TypeScript, Node.js",
-    },
-    {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      title: "Frontend developer",
       logo: ParabolLogo,
-      start: "2021",
+      start: "2023",
       end: "2024",
       description:
-        "Implemented new features, led a squad, improved code delivery process, and initiated migration from Emotion to Tailwind CSS. Technologies: React, TypeScript, GraphQL",
+        " Frontend development projects with nextjs",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
+      company: "Biletiniz",
+      link: "https://biletiniz.com",
+      badges: ["Onside"],
+      title: "Software Architect",
+      logo: "",
+      start: " 2023",
+      end: "2023",
+      description:
+"Full stack developer in biletiniz, technologies: Sequelize,nextjs,nodejs"
+    },
+ 
+    {
+      company: "Pnr technology",
+      link: "",
+      badges: ["Onside"],
+      title: "Wordpress developer",
+      logo: "",
+      start: "2022",
+      end: "2022",
+      description:
+      "Workking on wordpress project"
+    },
+    {
+      company: "GefeaSoft",
+      link: "",
+      badges: [],
+      title: "React and React native developer intern",
+      logo: null,
+      start: "2021",
       end: "2021",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, and AAA. Built a live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "worked web and mobile app",
     },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Led the Android team and developed apps for major Polish companies like LOT, Polskie Radio, Agora, and PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Developed and tested software for LTE base stations",
-    },
+
   ],
   skills: [
     "JavaScript",
     "TypeScript",
-    "React/Next.js/Remix",
+    "React/Next.js",
     "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "React Native",
+    "Golang",
+    
   ],
   projects: [
     {
-      title: "Parabol",
+      title: "",
       techStack: [
         "Full Stack Developer",
         "TypeScript",
